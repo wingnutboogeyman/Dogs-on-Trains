71cf23f69ee2176cb5f495379772ba30f284a932
wingnutboogeyman <84752530+wingnutboogeyman@users.noreply.github.com>
2022-03-27 15:09:22 -0400

    Add files via upload
    Initial upload.

---
+++ one_dog_train.py
@@ -0,0 +1,16 @@
+#duncan is my dog, the train he operates runs in mph
+dunk_speed = 30
+
+print(dunk_speed)
+
+#time traveling
+hours = [1, 2, 3, 4, 5]
+
+#travel distance in miles
+dunk_travel = 0
+
+print(dunk_travel)
+
+for x in hours:
+    dunk_travel += dunk_speed
+    print(f"{x} hours - {dunk_travel}")
