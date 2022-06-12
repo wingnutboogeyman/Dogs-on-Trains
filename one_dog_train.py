560941f870f327b378266d1a95e7f181fba6b32f
wingnutboogeyman <84752530+wingnutboogeyman@users.noreply.github.com>
2022-06-12 01:17:28 -0400

    Update one_dog_train.py
    This is more of what I pictured in my head.

--- one_dog_train.py
+++ one_dog_train.py
@@ -1,16 +1,20 @@
-#duncan is my dog, the train he operates runs in mph
+# Duncan is my dog, the train he operates runs in mph
 dunk_speed = 30
+dunk_speed_str = str(dunk_speed)
+print(f"Duncan's speed is {dunk_speed_str}")
 
-print(dunk_speed)
+# time traveling - LATER this will be an input field
+input_hours = 5
 
-#time traveling
-hours = [1, 2, 3, 4, 5]
-
-#travel distance in miles
+# travel distance in miles
 dunk_travel = 0
 
-print(dunk_travel)
+print("Distance Traveled:")
 
-for x in hours:
-    dunk_travel += dunk_speed
-    print(f"{x} hours - {dunk_travel}")
+counter = 1
+while counter <= input_hours:
+    dunk_distance = dunk_speed
+    dunk_travel += dunk_distance
+    print(f"Hour {counter}: {dunk_travel}")
+    counter += 1
+    
